343e4120e580f6e56811b2c657765024d031930b
Christian Niklas <christian.niklas@outletcity.com>
2025-09-27 17:32:39 +0200

    update camera button icon

--- src/App.vue
+++ src/App.vue
@@ -39,18 +39,23 @@ const { state, generateImage } = useHtmlToCanvas()
 		>
 			<svg
 				aria-hidden="true"
-				class="size-8 fill-current"
-				fill="#ffffff"
+				class="size-14"
 				viewBox="0 0 24 24"
 				width="32"
 				height="32"
-				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="1.125"
 			>
-				<circle cx="12" cy="12" r="3.2"></circle>
-				<path
-					d="M9 2L7.17 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2h-3.17L15 2H9zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"
-				/>
-				<path d="M0 0h24v24H0z" fill="none" />
+				<circle cx="12" cy="12" r="10" />
+				<path d="m14.31 8 5.74 9.94" />
+				<path d="M9.69 8h11.48" />
+				<path d="m7.38 12 5.74-9.94" />
+				<path d="M9.69 16 3.95 6.06" />
+				<path d="M14.31 16H2.83" />
+				<path d="m16.62 12-5.74 9.94" />
 			</svg>
 		</button>
 	</header>
